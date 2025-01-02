d01d2cdf2376253cb145d43fafdb5f6b21722611
mrpaulwoods <mr.paul.woods@gmail.com>
2025-01-01 20:01:37 -0600

    feat: bookmark entity

---
+++ src/main/java/org/mrpaulwoods/bookmarks/bookmark/Bookmark.java
@@ -0,0 +1,57 @@
+package org.mrpaulwoods.bookmarks.bookmark;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "url", nullable = false, length = 1000)
+    private String url;
+
+    public Bookmark() {
+    }
+
+    public Bookmark(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Bookmark bookmark = (Bookmark) o;
+        return getId() != null && Objects.equals(getId(), bookmark.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "name = " + name + ", " +
+                "url = " + url + ")";
+    }
+}
---
+++ src/main/java/org/mrpaulwoods/bookmarks/bookmark/BookmarkBootstrap.java
@@ -0,0 +1,25 @@
+package org.mrpaulwoods.bookmarks.bookmark;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BookmarkBootstrap implements CommandLineRunner {
+
+    private final BookmarkRepository bookmarkRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (bookmarkRepository.count() != 0L) {
+            return;
+        }
+
+        log.info("created bookmark {}", bookmarkRepository.save(new Bookmark("google", "https://google.com")));
+    }
+
+}
