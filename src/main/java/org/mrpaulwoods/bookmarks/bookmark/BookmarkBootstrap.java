f4ee6b5bb58cd0b8ba82681403f96cf4ce22828d
mrpaulwoods <mr.paul.woods@gmail.com>
2025-01-01 21:13:08 -0600

    fix: remove unnecessary exception declaration in BookmarkBootstrap
    The `run` method no longer throws any exceptions, so the `throws Exception` clause has been removed for cleaner and more accurate code. This simplifies the method signature and adheres to best practices.

--- src/main/java/org/mrpaulwoods/bookmarks/bookmark/BookmarkBootstrap.java
+++ src/main/java/org/mrpaulwoods/bookmarks/bookmark/BookmarkBootstrap.java
@@ -13,7 +13,7 @@ public class BookmarkBootstrap implements CommandLineRunner {
     private final BookmarkRepository bookmarkRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         if (bookmarkRepository.count() != 0L) {
             return;
